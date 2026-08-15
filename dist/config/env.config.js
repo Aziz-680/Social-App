440df77e479a06f9c834168124b015e7890186ee
Aziz-680 <abdelazizhisham8@gmail.com>
2026-08-15 15:55:35 +0300

    MongoDB config

--- dist/config/env.config.js
+++ dist/config/env.config.js
@@ -3,7 +3,8 @@ var _a, _b, _c;
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)({
-    path: [`.${process.env.NODE_ENV}.env`, '.env']
+    // path:[ `.${process.env.NODE_ENV}.env` , '.env' ]
+    path: '.dev.env'
 });
 const envConfig = {
     app: {
